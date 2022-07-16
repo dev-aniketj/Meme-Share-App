bd8bc83ea97221dbe36a7ddc75617d62bf6c8bae
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-16 18:23:23 +0530

    master

--- app/src/main/java/com/aniketjain/memeshare/HomeActivity.kt
+++ app/src/main/java/com/aniketjain/memeshare/HomeActivity.kt
@@ -2,7 +2,12 @@ package com.aniketjain.memeshare
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.aniketjain.memeshare.databinding.ActivityHomeBinding
+import com.aniketjain.roastytoasty.Toasty
+import com.bumptech.glide.Glide
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -14,16 +19,36 @@ class HomeActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        loadMeme()
         onClicksListeners()
 
     }
 
     private fun onClicksListeners() {
         binding.shareBtn.setOnClickListener { }
-        binding.nextBtn.setOnClickListener { }
+        binding.nextBtn.setOnClickListener { loadMeme() }
     }
 
-    private fun setUpImage() {
+    private fun loadMeme() {
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
 
+        // Request a JSON response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            { response ->
+                val url = response.get("url")
+                Glide.with(this).load(url).into(binding.image)
+            },
+            {
+                Toasty.error(this, "Something went wrong.")
+            }
+        )
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }

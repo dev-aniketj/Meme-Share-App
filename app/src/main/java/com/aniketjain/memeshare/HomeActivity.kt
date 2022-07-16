e34e2ccc22da8b1154b699f7b620dcb6d8ef3750
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-16 18:46:24 +0530

    master

--- app/src/main/java/com/aniketjain/memeshare/HomeActivity.kt
+++ app/src/main/java/com/aniketjain/memeshare/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.aniketjain.memeshare
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
@@ -8,6 +10,10 @@ import com.android.volley.toolbox.Volley
 import com.aniketjain.memeshare.databinding.ActivityHomeBinding
 import com.aniketjain.roastytoasty.Toasty
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -38,7 +44,28 @@ class HomeActivity : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { response ->
                 val url = response.get("url")
-                Glide.with(this).load(url).into(binding.image)
+                Glide.with(this).load(url).listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.VISIBLE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+                }).into(binding.image)
             },
             {
                 Toasty.error(this, "Something went wrong.")
@@ -46,9 +73,5 @@ class HomeActivity : AppCompatActivity() {
         )
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest)
-    }
-
-    override fun onStart() {
-        super.onStart()
     }
 }

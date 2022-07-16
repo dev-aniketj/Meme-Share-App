da0d026c2bad55b62679e94b571449cbcd55be5b
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-16 20:40:45 +0530

    master

--- app/src/main/java/com/aniketjain/memeshare/HomeActivity.kt
+++ app/src/main/java/com/aniketjain/memeshare/HomeActivity.kt
@@ -1,14 +1,15 @@
 package com.aniketjain.memeshare
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.aniketjain.memeshare.databinding.ActivityHomeBinding
-import com.aniketjain.roastytoasty.Toasty
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -17,6 +18,7 @@ import com.bumptech.glide.request.target.Target
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private var currentImageURL: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,20 +33,32 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun onClicksListeners() {
-        binding.shareBtn.setOnClickListener { }
-        binding.nextBtn.setOnClickListener { loadMeme() }
+        binding.shareBtn.setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                intent.putExtra(
+                    Intent.EXTRA_TEXT,
+                    "Hey, Checkout this cool meme I got from Reddit $currentImageURL"
+                )
+            }
+            startActivity(Intent.createChooser(intent, "Share this meme using..."))
+        }
+        binding.nextBtn.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+            loadMeme()
+        }
     }
 
     private fun loadMeme() {
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
-
         // Request a JSON response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { response ->
-                val url = response.get("url")
-                Glide.with(this).load(url).listener(object : RequestListener<Drawable> {
+                currentImageURL = response.getString("url")
+                Glide.with(this).load(currentImageURL).listener(object : RequestListener<Drawable> {
                     override fun onLoadFailed(
                         e: GlideException?,
                         model: Any?,
@@ -52,6 +66,7 @@ class HomeActivity : AppCompatActivity() {
                         isFirstResource: Boolean
                     ): Boolean {
                         binding.progressBar.visibility = View.VISIBLE
+                        binding.image.visibility = View.GONE
                         return false
                     }
 
@@ -63,12 +78,13 @@ class HomeActivity : AppCompatActivity() {
                         isFirstResource: Boolean
                     ): Boolean {
                         binding.progressBar.visibility = View.GONE
+                        binding.image.visibility = View.VISIBLE
                         return false
                     }
                 }).into(binding.image)
             },
             {
-                Toasty.error(this, "Something went wrong.")
+                Toast.makeText(this, "Something went wrong.", Toast.LENGTH_SHORT).show()
             }
         )
         // Add the request to the RequestQueue.
